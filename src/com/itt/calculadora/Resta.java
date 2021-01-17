cb2ae42a7f7e593d291081581409a81a769fdae9
JoseASantoss <j.santos.sanchez@gmail.com>
2021-01-17 19:08:07 +0100

    Actualización clase suma y resta

--- src/com/itt/calculadora/Resta.java
+++ src/com/itt/calculadora/Resta.java
@@ -1,5 +1,56 @@
 package com.itt.calculadora;
 
-public class Resta {
+import java.text.NumberFormat;
+import java.util.Locale; 
 
+public class Resta {
+	
+	private double num1, num2, num3;
+	private double acum;
+	private double result;
+	
+	public Resta() {
+		
+		this.num1 = 0;
+		this.num2 = 0;
+		this.num3 = 0;
+		this.acum = 0;
+		this.result = 0;
+		
+	}
+	
+	public String resReales(double num1, double num2) {
+		 
+		this.result = num1 + num2;
+		
+		NumberFormat nf = NumberFormat.getInstance(new Locale("es", "ES"));
+		
+		return nf.format(this.result);
+		
+	}
+	
+	public String resEnteros(int num1, int num2) {
+		 
+		this.result = num1 + num2;
+		
+		NumberFormat nf = NumberFormat.getInstance(new Locale("es", "ES"));
+		
+		return  nf.format(this.result);
+		
+	}
+	
+	public String resTresReales(double num1, double num2, double num3) {
+		
+		this.result = num1 + num2 + num3;
+		
+		NumberFormat nf = NumberFormat.getInstance(new Locale("es", "ES"));
+		
+		return nf.format(this.result);
+	}
+	
+	public void resAcumulado(double num) {
+		
+		this.acum = acum + num;
+	}
+	
 }
--- src/com/itt/calculadora/Suma.java
+++ src/com/itt/calculadora/Suma.java
@@ -1,5 +1,56 @@
 package com.itt.calculadora;
 
-public class Suma {
+import java.text.NumberFormat;
+import java.util.Locale; 
 
+public class Suma {
+	
+	private double num1, num2, num3;
+	private double acum;
+	private double result;
+	
+	public Suma() {
+		
+		this.num1 = 0;
+		this.num2 = 0;
+		this.num3 = 0;
+		this.acum = 0;
+		this.result = 0;
+		
+	}
+	
+	public String sumReales(double num1, double num2) {
+		 
+		this.result = num1 + num2;
+		
+		NumberFormat nf = NumberFormat.getInstance(new Locale("es", "ES"));
+		
+		return nf.format(this.result);
+		
+	}
+	
+	public String sumEntero(int num1, int num2) {
+		 
+		this.result = num1 + num2;
+		
+		NumberFormat nf = NumberFormat.getInstance(new Locale("es", "ES"));
+		
+		return  nf.format(this.result);
+		
+	}
+	
+	public String sumTresReales(double num1, double num2, double num3) {
+		
+		this.result = num1 + num2 + num3;
+		
+		NumberFormat nf = NumberFormat.getInstance(new Locale("es", "ES"));
+		
+		return nf.format(this.result);
+	}
+	
+	public void sumAcumulado(double num) {
+		
+		this.acum = acum + num;
+	}
+	
 }
