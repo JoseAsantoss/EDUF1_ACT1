d58692308dc1bf12af35fc5a320866cf48925a1b
JoseASantoss <j.santos.sanchez@gmail.com>
2021-01-19 21:30:06 +0100

    actualizado main

---
+++ src/com/itt/calculadora/Calculadora.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.itt.calculadora;
+
+/**
+ * @author jsant
+ *
+ */
+public class Calculadora {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
