984157e659b3b8cc4e3c61eeef87fa56c3cce07d
Migue <miguelartedigital@gmail.com>
2021-01-21 18:19:18 +0100

    Nueva Division

--- src/com/itt/calculadora/Division.java
+++ src/com/itt/calculadora/Division.java
@@ -3,24 +3,51 @@ package com.itt.calculadora;
 import java.text.NumberFormat;
 import java.util.Locale;
 
+
+/**
+ * 
+ * Esta clase está creada para realizar la división entre dos números.
+ * 
+ * El usuario introducirá dos números (enteros, reales) para realizar
+ * las distintas operaciones solicitadas.
+ * 
+ * Se le advertirá que el segundo número debe ser distinto de cero,
+ * de introducir cero le saldría un mensaje advirtiendo que no es correcto.
+ * 
+ * 
+ * @author Miguel A. Lozano.
+ * @since 20-01-2021.
+ * @version 1.0
+ */
+
 public class Division {
 	
 	private static double result;
 	
-	public Division() {
+	public Division() { // constructor
 		
 		result = 0;
 		
 	}
 	
-	public static String divReales(double num1, double num2) {
+	/**
+	  * <b>Método para dividir dos números reales.</b>
+	  * @param num1 recoge el primer número, que será el dividendo de la división.
+	  * @param num2 recoge el segundo número, que será el divisor de la división,
+	  * este número deberá ser <b>distinto de cero</b>. De poner un numero negativo
+	  * 
+	  * @return nos devuelve (retorna) el resultado de la división.
+	  *  
+	  * @throws ¿?
+	  */
+	
+	public static Double divReales(double num1, double num2) {
 		
-		result = num1/num2;
-		
-		NumberFormat nf = NumberFormat.getNumberInstance(new Locale("es", "ES"));
+		return (num1/num2);
 		 
-		return nf.format(result);
 	}
+	
+	
 	
 	public static String divEnteros(int num1, int num2) {
 		
@@ -39,7 +66,22 @@ public class Division {
 		 
 		return nf.format(result);
 	}
+	/**
+	 * 
+	 * 
+	 * {@link Division#raiz()}
+	 * 
+	 * #
+	 * 
+	 * 
+	 */
 	
+	/**
+	 * #
+	 * 
+	 * @param num
+	 * @return
+	 */
 	public static String raiz(int num) {
 	
 		result = Math.sqrt(num);
