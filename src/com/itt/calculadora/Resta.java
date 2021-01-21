2ba3ee90c16ba938c4d143271909c14b5a79ff3f
Raúl Ruiz Sanz <raulrusa2@gmail.com>
2021-01-21 16:12:40 +0100

    Añadidos links en resAcumulado, getAcumulado y cambios en la redacción de comentarios Javadoc

--- src/com/itt/calculadora/Resta.java
+++ src/com/itt/calculadora/Resta.java
@@ -15,7 +15,7 @@ public class Resta {
 	/**
 	 * acum: acumula resultado de diferentes restas
 	 */
-	private static double acum=0;
+	private static double acum = 0;
 
 	/**
 	 * Constructor de la clase Resta
@@ -24,11 +24,11 @@ public class Resta {
 	}
 
 	/**
-	 * Realiza la resta de sus argumentos
+	 * Realiza la resta de dos números reales
 	 * 
 	 * @param num1 este es el minuendo de la resta
 	 * @param num2 este es el sustraendo de la resta
-	 * @return devuelve la resta de num1 menos num2.
+	 * @return devuelve la resta de num1 menos num2
 	 */
 	public static double resReales(double num1, double num2) {
 
@@ -36,11 +36,11 @@ public class Resta {
 	}
 
 	/**
-	 * Realiza la resta de sus argumentos
+	 * Realiza la resta de dos números enteros
 	 * 
 	 * @param num1 este es el minuendo de la resta
 	 * @param num2 este es el sustraendo de la resta
-	 * @return devuelve la resta de num1 menos num2.
+	 * @return devuelve la resta de num1 menos num2
 	 * 
 	 */
 	public static int resEnteros(int num1, int num2) {
@@ -50,12 +50,12 @@ public class Resta {
 	}
 
 	/**
-	 * Realiza la resta del primer parámetro menos los otros dos
+	 * Realiza la resta del de tres números reales
 	 * 
 	 * @param num1 este es el minuendo de la resta
 	 * @param num2 este es el primer sustraendo de la resta
 	 * @param num3 este es el segundo sustraendo de la resta
-	 * @return devuelve el resultado de la resta de (num1 menos num2) menos num3.
+	 * @return devuelve el resultado de la resta de (num1 menos num2) menos num3
 	 */
 	public static double resTresReales(double num1, double num2, double num3) {
 
@@ -63,22 +63,26 @@ public class Resta {
 	}
 
 	/**
+	 * Acumula el valor que pasamos como argumento</br>
+	 * Para consultar el valor acumulado de las restas usamos
+	 * {@link Resta#getAcum()}
 	 * 
-	 * @param num parámetro con el resultado de las restas que queremos ir acumulando
+	 * @param num resultado de las restas que queremos ir acumulando
 	 */
-	public void resAcumulado(double num) {
+	public static void resAcumulado(double num) {
 
 		acum = acum + num;
 
 	}
+
 	/**
+	 * Devuelve el valor de las restas que hemos ido acumulando con el método
+	 * {@link #resAcumulado(double)}
 	 * 
-	 * @return
-	 * 		Devuelve el valor de las restas que hemos ido acumulando con el método @see resAcumulado.
+	 * @return Devuelve valor acumulado
 	 */
 	public static double getAcum() {
 		return acum;
 	}
-	
-	
+
 }
