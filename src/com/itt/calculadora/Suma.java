9a5fbf16893fbe0bd93667b1d7180bcc15188a58
JoseAsantoss <j.santos.sanchez@gmail.com>
2021-01-22 10:42:28 +0100

    V1.0 Javadoc Suma

--- src/com/itt/calculadora/Suma.java
+++ src/com/itt/calculadora/Suma.java
@@ -2,56 +2,56 @@ package com.itt.calculadora;
 
 
 /**
- * <h2>Clase Suma:</h2>
- * <br>
- * <p>Clase estática que realiza las operaciones básicas de suma en una calculadora: <br>
- * - suma de dos números <br>
- * - suma de tres números <br>
- * - suma con memoria</p>
+ * <p>Clase est&aacutetica que realiza las operaciones b&aacutesicas de suma en una calculadora: <br>
+ * &nbsp&nbsp- suma de dos n&uacutemeros <br>
+ * &nbsp&nbsp- suma de tres n&uacutemeros <br>
+ * &nbsp&nbsp- suma con memoria<br>
+ * No est&aacute permitido usar n&uacutemeros negativos en la suma. Su uso inducir&aacute a un error
+ * en la aplicaci&oacuten</p>
  * <hr/>
  * <h3>Metodos que implementa:</h3>
  * <br>
  * <p>
  * 	<ol>
- * 			<li><h3><em>public.Suma():</em></h3>
+ * 			<li><h3><em>{@link #Suma()}:</em></h3>
  * 				<ul>
  * 					<li>
  * 						Constructor por defecto que no recibe argumentos.
  * 					</li>
  * 				</ul>
  * 			</li>
- * 			<li><h3><em>sumReales(double num1, double num2):</em></h3>
+ * 			<li><h3><em>{@link #sumReales(double, double)}:</em></h3>
  * 				<ul>
  * 					<li>
- * 						Método para la suma de dos números reales.
+ * 						M&eacutetodo para la suma de dos n&uacutemeros reales.
  * 					</li>
  * 				</ul>
  * 			</li>
- * 			<li><h3><em>sumEnteros(int num1, int num2):</em></h3>
+ * 			<li><h3><em>{@link #sumEntero(int, int)}:</em></h3>
  * 				<ul>
  * 					<li>
- * 						Método para la suma de dos números enteros.
+ * 						M&eacutetodo para la suma de dos n&uacutemeros enteros.
  * 					</li>
  * 				</ul>
  * 			</li>
- * 			<li><h3><em>sumReales(double num1, double num2, double num3):</em></h3>
+ * 			<li><h3><em>{@link #sumTresReales(double, double, double)}:</em></h3>
  * 				<ul>
  * 					<li>
- * 						Método para la suma de tres números reales.
+ * 						M&eacutetodo para la suma de tres n&uacutemeros reales.
  * 					</li>
  * 				</ul>
  * 			</li>
- * 			<li><h3><em>void sumAcumulado(double num):</em></h3>
+ * 			<li><h3><em>{@link #sumAcumulado(double)}:</em></h3>
  * 				<ul>
  * 					<li>
- * 						Método para la suma con memoria.
+ * 						M&eacutetodo para la suma con memoria.
  * 					</li>
  * 				</ul>
  * 			</li>
- * 			<li><h3><em>getAcum():</em></h3>
+ * 			<li><h3><em>{@link #getAcum()}:</em></h3>
  * 				<ul>
  * 					<li>
- * 						Método para poder mostar el resultado de la suma acumulada
+ * 						M&eacutetodo para poder mostar el resultado de la suma acumulada
  * 					</li>
  *	 			</ul>
  * 			</li>
@@ -60,17 +60,16 @@ package com.itt.calculadora;
  *<hr/>
  *<h2>Casos especiales:</h2>
  *<ol>
- *	<li><p>Utilización de números negativos no permitidos:</p>
- * 		<dd>Si uno de los números pasados es negativo se lanzara una excepción
- *		ya que realiza operaciones básicas de suma de números positivos.</dd>
+ *	<li><p>Utilizaci&oacuten de n&uacutemeros negativos no permitidos:</p>
+ * 		<dd>Si uno de los n&uacutemeros pasados es negativo se lanzara una excepci&oacuten
+ *		ya que realiza operaciones b&aacutesicas de suma de n&uacutemeros positivos.</dd>
  * 	</li>
- * 	<li><p>Usar caracteres en vez de números:
- * 		<dd>Si en vez de números como argumentos se pasan caracteres, se lanzará
- * 		una excepción ya que lo único que están permitidos son números.</dd>
+ * 	<li><p>Usar caracteres en vez de n&uacutemeros:
+ * 		<dd>Si en vez de n&uacutemeros como argumentos se pasan caracteres, se lanzar&aacute
+ * 		una excepci&oacuten ya que lo &uacutenico que est&aacuten permitidos son n&uacutemeros.</dd>
  * 	</li>
  *</ol>
  *
- * @category Funciones matemáticas
  * @version 1.0
  * @since 20/01/2021
  * @author jass
@@ -80,22 +79,22 @@ public class Suma {
 	private static double acum = 0;
 	
 	/**
-	 * Constructor por defecto que no recibe ningún parámetro.
+	 * Constructor por defecto que no recibe ning&uacuten par&aacutemetro.
 	 */
 	public Suma() {
 		
 	}
 	
 	/**
-	 * Método estático que recibe dos números reales como argumentos y devuelve la suma de ambos.
+	 * M&eacutetodo est&aacutetico que recibe dos n&uacutemeros reales como argumentos y devuelve la suma de ambos.
 	 * @param num1
-	 * 		Sumando que recibe un número real
+	 * 		Sumando que recibe un n&uacutemero real
 	 * @param num2
-	 * 		Sumando que recibe un número real
+	 * 		Sumando que recibe un n&uacutemero real
 	 * @return
-	 * 		Un número real que es el resultado de la suma de num1 + num2.
+	 * 		Un n&uacutemero real que es el resultado de la suma de num1 + num2.
 	 * @exception
-	 * 		IOException Si en uno de los parámetros en vez de un número se pasa un carácter lanzará una excepción
+	 * 		IOException Si en uno de los par&aacutemetros en vez de un n&uacutemero se pasa un car&aacutecter lanzar&aacute una excepci&oacuten
 	 * 		indicando el error
 	 */
 	public static double sumReales(double num1, double num2) {
@@ -105,7 +104,8 @@ public class Suma {
 	}
 	
 	/**
-	 * Método estático que recibe dos números enteros como argumentos y devuelve la suma de ambos.
+	 * M&eacutetodo est&aacutetico que recibe dos números enteros como argumentos y 
+	 * devuelve la suma de ambos.
 	 * @param num1
 	 * 		Sumando que recibe un número entero
 	 * @param num2
@@ -113,7 +113,7 @@ public class Suma {
 	 * @return
 	 * 		El resultado de la suma de num1 y num2.
 	 * @exception
-	 * 		IOException Si en uno de los parámetros en vez de un número se pasa un carácter lanzará una excepción
+	 * 		IOException Si en uno de los par&aacutemetros en vez de un n&uacutemero se pasa un car&aacutecter lanzar&aacute una excepci&oacuten
 	 * 		indicando el error
 	 */
 	public static int sumEntero(int num1, int num2) {
@@ -123,17 +123,17 @@ public class Suma {
 	}
 	
 	/**
-	 * Método estático que recibe tres números reales como argumentos y devuelve la suma de ambos.
+	 * M&eacutetodo est&aacutetico que recibe tres n&uacutemeros reales como argumentos y devuelve la suma de ambos.
 	 * @param num1
-	 * 		Sumando que recibe un número real
+	 * 		Sumando que recibe un n&uacutemero real
 	 * @param num2
-	 * 		Sumando que recibe un número real
+	 * 		Sumando que recibe un n&uacutemero real
 	 * @param num3
-	 * 		Sumando que recibe un número real
+	 * 		Sumando que recibe un n&uacutemero real
 	 * @return
-	 * 		Un número real que es el resultado de la suma de num1, num2 y num3
+	 * 		Un n&uacutemero real que es el resultado de la suma de num1, num2 y num3
 	 * @exception
-	 * 		IOException Si en uno de los parámetros en vez de un número se pasa un carácter lanzará una excepción
+	 * 		IOException Si en uno de los par&aacutemetros en vez de un n&uacutemero se pasa un car&aacutecter lanzar&aacute una excepci&oacuten
 	 * 		indicando el error
 	 */
 	public static double sumTresReales(double num1, double num2, double num3) {
@@ -143,25 +143,25 @@ public class Suma {
 	}
 	
 	/**
-	 * Método estático que acumula el número que se vaya pasando 
-	 * como parámetro, con el fin de realizar una suma acumulada.
+	 * M&eacutetodo est&aacutetico que acumula el n&uacutemero que se vaya pasando 
+	 * como par&aacutemetro, con el fin de realizar una suma acumulada.
 	 * @param num
-	 * 		Número de tipo real 
+	 * 		N&uacutemero de tipo real 
 	 * @exception
-	 * 		IOException Si en uno de los parámetros en vez de un número se pasa un carácter lanzará una excepción
+	 * 		IOException Si en uno de los par&aacutemetros en vez de un n&uacutemero se pasa un car&aacutecter lanzar&aacute una excepci&oacuten
 	 * 		indicando el error
 	 */
 	public static void sumAcumulado(double num) {
 
-		//TODO Aquí el código que tiene que ir acumulando la suma.
+		//TODO Aqu&iacute el c&oacutedigo que tiene que ir acumulando la suma.
 		
 	}
 	/**
-	 * Método estático que no recibe paŕametros y devuelve el valor acumulado de la suma.
+	 * M&eacutetodo est&aacutetico que no recibe par&aacutemetros y devuelve el valor acumulado de la suma.
 	 * @return
-	 * 		Devuelve el valor acumulado de la suma.
+	 * 		Devuelve el acumulado de la suma.
 	 * @exception
-	 * 		IOException Si en uno de los parámetros en vez de un número se pasa un carácter lanzará una excepción
+	 * 		IOException Si en uno de los par&aacutemetros en vez de un n&uacutemero se pasa un car&aacutecter lanzar&aacute una excepci&oacuten
 	 * 		indicando el error
 	 */
 	public static double getAcum() {
